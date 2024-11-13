6335e4885fdd10130e7b850b74e2f9ed6c53efde
robda20188 <danielrouco188rr@gmail.com>
2024-11-13 19:01:58 +0100

    Adding PeekViewer to preview markdown in nvim
    To open peekviewer in nvim type :PeekOpen and to close it type
    :PeekClose

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,12 +37,23 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
 	{'ojroques/nvim-hardline'},
+
 	{
-    "scottmckendry/cyberdream.nvim",
+	"scottmckendry/cyberdream.nvim",
     lazy = false,
     priority = 1000,
-}
+	},
 
+	{
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+	},
 }
   },{
   -- Configure any other settings here. See the documentation for more details.
@@ -52,5 +63,31 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
+--SETUPS
 require('hardline').setup {}
+-- default config:
+require('peek').setup({
+  auto_load = true,         -- whether to automatically load preview when
+                            -- entering another markdown buffer
+  close_on_bdelete = true,  -- close preview window on buffer delete
+
+  syntax = true,            -- enable syntax highlighting, affects performance
+
+  theme = 'dark',           -- 'dark' or 'light'
+
+  update_on_change = true,
+
+  app = 'browser',          -- 'webview', 'browser', string or a table of strings
+                            -- explained below
+
+  filetype = { 'markdown' },-- list of filetypes to recognize as markdown
+
+  -- relevant if update_on_change is true
+  throttle_at = 200000,     -- start throttling when file exceeds this
+                            -- amount of bytes in size
+  throttle_time = 'auto',   -- minimum amount of time in milliseconds
+                            -- that has to pass before starting new render
+})
+
+-- SET COLORSCHEME
 vim.cmd('colorscheme cyberdream')
